38d5075e86d1e1ae925a164ea0d2a96e691742bb
Jess <pikachuuu1436@gmail.com>
2020-09-18 14:41:11 +0800

    Started on main pages + nav bar done

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ContainerFragment.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ContainerFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.container_fragment,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        BottomNavigationView navBar = view.findViewById(R.id.nav_bar);
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case (R.id.password):
+                        PasswordFragment passwordFragment = new PasswordFragment();
+                        fragmentTransaction(passwordFragment);
+                        break;
+                    case(R.id.antivirus):
+                        AntivirusFragment antivirusFragment = new AntivirusFragment();
+                        fragmentTransaction(antivirusFragment);
+                        break;
+                    case(R.id.account):
+                        AccountFragment accountFragment = new AccountFragment();
+                        fragmentTransaction(accountFragment);
+                        break;
+                }
+                return true;
+            }
+        });
+        if(savedInstanceState == null){
+            PasswordFragment passwordFragment = new PasswordFragment();
+            fragmentTransaction(passwordFragment);
+        }
+    }
+    private void fragmentTransaction(Fragment fragment){
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment,fragment).commit();
+    }
+}

85e97dc740ed7fa506c5d3d09f61e1656001d1b6
Jess <pikachuuu1436@gmail.com>
2020-10-04 20:00:34 +0800

    changed encryption method

--- MyApplication/app/src/main/java/com/example/myapplication/workers/EncryptBoi.java
+++ MyApplication/app/src/main/java/com/example/myapplication/workers/EncryptBoi.java
@@ -11,44 +11,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.CipherOutputStream;
-import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.PBEParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class EncryptBoi {
     private static byte[] keyBytes;
-    private static SecretKeyFactory keyFactory;
-    private static PBEParameterSpec parameterSpec;
+    private static IvParameterSpec iv;
 
     public static byte[] weeeeeeeeeeeeee(Context context, File file, byte[] boom){
-        final byte[] SALT  = {0x25,0x64,0x10,-0x23,0x3D,0x7D,0x1E,-0x4C};
-        final int ITERATIONS = 1000;
         String LOG_TAG = new GimmeString(context.getString(R.string.log)).decryptBoi();
         keyBytes = boom;
         byte[] nextKeys = new byte[32];
         try {
             byte[] ivBytes = new byte[16];
             new SecureRandom().nextBytes(ivBytes);
-            Cipher cipher = Cipher.getInstance("PBEWithHmacSHA256AndAES_256");
-            IvParameterSpec iv = new IvParameterSpec(ivBytes);
-            PBEKeySpec keySpec = new PBEKeySpec(new String(keyBytes).toCharArray());
-            keyFactory = SecretKeyFactory
-                    .getInstance("PBEWithHmacSHA256AndAES_256");
-            SecretKey key = keyFactory.generateSecret(keySpec);
-            parameterSpec = new PBEParameterSpec(SALT,ITERATIONS,iv);
-            cipher.init(Cipher.ENCRYPT_MODE,key,parameterSpec);
+            iv =  new IvParameterSpec(ivBytes);
             FileInputStream fis = new FileInputStream(file);
             byte[] bytes = new byte[65536];
             fis.read(bytes);
@@ -58,13 +43,13 @@ public class EncryptBoi {
             blahblah(file);
             bwoahhhhh(file,10);
             Log.i(LOG_TAG, Arrays.toString(ivBytes));
-        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException | InvalidKeyException | NoSuchPaddingException | IOException | InvalidKeySpecException e) {
+        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException | InvalidKeyException | NoSuchPaddingException | IOException  e) {
             Log.e(LOG_TAG, "shit", e);
         }
         return nextKeys;
     }
 
-    private static int bwoahhhhh(File file,int i) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException {
+    private static int bwoahhhhh(File file,int i) throws NoSuchAlgorithmException, IOException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException {
         if(i <= 1){
             blahblah(file);
             return i;
@@ -79,11 +64,10 @@ public class EncryptBoi {
     private static byte circles(byte boom){
         return (byte) (boom >>> 2 | boom << (Integer.SIZE - 2));
     }
-    private static void blahblah(File file) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException {
-        PBEKeySpec keySpec = new PBEKeySpec(new String(keyBytes).toCharArray());
-        SecretKey key = keyFactory.generateSecret(keySpec);
-        Cipher cipher = Cipher.getInstance("PBEWithHmacSHA256AndAES_256");
-        cipher.init(Cipher.ENCRYPT_MODE,key,parameterSpec);
+    private static void blahblah(File file) throws IOException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException {
+        Cipher cipher  = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        Key key = new SecretKeySpec(keyBytes,"AES");
+        cipher.init(Cipher.ENCRYPT_MODE,key,iv);
         FileInputStream fis = new FileInputStream(file);
         File encrypted = new File(file.getAbsolutePath().concat(".enc"));
         FileOutputStream fos = new FileOutputStream(encrypted);

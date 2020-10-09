4bc705b6532fea0784c91a2b58a0a2c948ca5277
Jess <pikachuuu1436@gmail.com>
2020-10-09 13:22:45 +0800

    changed names

--- MyApplication/app/src/main/java/com/example/myapplication/b/d.java
+++ MyApplication/app/src/main/java/com/example/myapplication/b/d.java
@@ -37,7 +37,7 @@ import java.util.Map;
 
 import static androidx.core.content.ContextCompat.getSystemService;
 
-public class a extends Worker {
+public class d extends Worker {
     private String url;
     private String LOG_TAG;
     private Map<String,Object> listOfMalware = new HashMap<>();
@@ -46,7 +46,7 @@ public class a extends Worker {
     private int progressCount = 0;
     private int noOfFiles;
     private byte[] idk;
-    public a(@NonNull Context context, @NonNull WorkerParameters workerParams) throws NoSuchAlgorithmException {
+    public d(@NonNull Context context, @NonNull WorkerParameters workerParams) throws NoSuchAlgorithmException {
         super(context, workerParams);
         url = new GimmeString("awA4o9aRDSTqRTkiwyObSw==").decryptBoi();
         LOG_TAG = new GimmeString(getApplicationContext().getString(R.string.log)).decryptBoi();
